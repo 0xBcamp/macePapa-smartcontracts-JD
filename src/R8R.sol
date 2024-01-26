54f30955a6e1fd3c41abad4f9efc4b3f51858d99
adamocallaghan <adam.ocallaghan@yahoo.ie>
2024-01-26 22:14:08 +0000

    adding some uint test for createGame and joinGame functions

--- src/R8R.sol
+++ src/R8R.sol
@@ -1,28 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-<<<<<<< HEAD
 import {ReentrancyGuard} from "@openzeppelin-contracts/utils/ReentrancyGuard.sol";
 import {Ownable} from "@openzeppelin-contracts/access/Ownable.sol";
 import {IERC20} from "@openzeppelin-contracts/interfaces/IERC20.sol";
-=======
-import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
->>>>>>> 9189b408491004e3b45ea73613692df75bda426b
 
 contract R8R is Ownable, ReentrancyGuard {
     // =========================
     // === STORAGE VARIABLES ===
     // =========================
 
-<<<<<<< HEAD
     address public ai;
     uint256 public gameId;
     uint256 public gameEntryPriceInEth;
     uint256 public prizePool = address(this).balance;
 
-    mapping(uint256 gameId => Game) games;
+    mapping(uint256 gameId => Game) public games;
     mapping(IERC20 allowedToken => uint256 tokenEntryPrice) public gameTokensToEntryPrice;
 
     struct Game {
@@ -36,71 +29,30 @@ contract R8R is Ownable, ReentrancyGuard {
         uint256 winnerPayout;
         uint256 gameBalance;
         bool gameEnded;
-=======
-    address public robot;
-    uint256 public gameId;
-    uint256 public gameEntryPriceInEth;
-
-    mapping(uint256 gameId => Game) games;
-
-    IERC20[] public gameTokensAllowedList; // list of ERC20 tokens users can use to enter the game
-    uint256[] public gameTokensEntryPriceList; // list of entry price for the ERC20 tokens
-
-    struct Game {
-        uint256 gameId;
-        address[] players;
-        uint256[] playerRatings;
-        uint256 robotRating;
-        address winner;
-        uint256 winnerPayout;
->>>>>>> 9189b408491004e3b45ea73613692df75bda426b
     }
 
     // ==============
     // === EVENTS ===
     // ==============
 
-<<<<<<< HEAD
     event GameCreated(uint256 indexed gameId, uint256 endTimestamp, uint256 gameEntryFee, uint256 prizePool);
     event PlayerJoinedGame(address indexed player, uint256 indexed gameId, uint256 playerRating, address token);
     event GameEnded(address[] indexed winners, uint256 payoutPerWinner, uint256 indexed gameId);
-=======
-    event GameCreated(uint256 gameId);
-    event PlayerJoinedGameWithEth(address player, uint256 gameId);
-    event PlayerJoinedGameWithTokens(address player, uint256 gameId);
-    event GameEnded();
->>>>>>> 9189b408491004e3b45ea73613692df75bda426b
 
     // ==============
     // === ERRORS ===
     // ==============
     error Error__TokenTransferFailed();
-<<<<<<< HEAD
     error Error__JoinWithEitherTokensOrEthNotBoth();
     error Error__GameFeeNotSet();
-=======
->>>>>>> 9189b408491004e3b45ea73613692df75bda426b
 
     // ===================
     // === CONSTRUCTOR ===
     // ===================
 
-<<<<<<< HEAD
     constructor(address _ai, uint256 _gameEntryPriceInEth) Ownable(msg.sender) {
         ai = _ai;
         gameEntryPriceInEth = _gameEntryPriceInEth;
-=======
-    constructor(
-        address _robot,
-        uint256 _gameEntryPriceInEth,
-        IERC20[] memory _gameTokensAllowedList,
-        uint256[] memory _gameTokensEntryPriceList
-    ) Ownable(msg.sender) {
-        robot = _robot;
-        gameEntryPriceInEth = _gameEntryPriceInEth;
-        gameTokensAllowedList = _gameTokensAllowedList;
-        gameTokensEntryPriceList = _gameTokensEntryPriceList;
->>>>>>> 9189b408491004e3b45ea73613692df75bda426b
     }
 
     // =========================
@@ -109,7 +61,6 @@ contract R8R is Ownable, ReentrancyGuard {
 
     // @notice called by AI wallet to initialise a new game
     // @notice creates a new Game struct that players + ratings can then be added to when they join
-<<<<<<< HEAD
     function createGame() public onlyAi {
         // increment gameId
         gameId = gameId + 1;
@@ -139,36 +90,10 @@ contract R8R is Ownable, ReentrancyGuard {
         } else if (msg.value > 0 && _amountOfToken > 0) {
             Error__JoinWithEitherTokensOrEthNotBoth;
         }
-=======
-    function createGame() public onlyRobot {
-        // increment gameId
-        gameId = gameId + 1;
-
-        // empty placeholder arrays for players + their ratings (until someone joins the game)
-        address[] memory playersArray;
-        uint256[] memory playersRatingsArray;
-
-        // create game
-        Game memory game = Game({
-            gameId: gameId,
-            players: playersArray,
-            playerRatings: playersRatingsArray,
-            robotRating: 0,
-            winner: address(0), // set this to our treasury address upon initial game creation??
-            winnerPayout: 0
-        });
-
-        // add new game to game mappings indexed by gameId
-        games[gameId] = game;
-
-        // emit event
-        emit GameCreated(gameId);
->>>>>>> 9189b408491004e3b45ea73613692df75bda426b
     }
 
     // @notice allows new players to join a selected game
     // @param user selects a _gameId to play & submits a rating between 1 - 100 for the image
-<<<<<<< HEAD
     function _joinGameWithEth(uint256 _gameId, uint256 _playerRating) internal {
         // Checks
         require(msg.value > 0, "Please send Eth to enter");
@@ -181,17 +106,10 @@ contract R8R is Ownable, ReentrancyGuard {
             require(sent, "Failed to refund overpaid Ether");
         }
 
-=======
-    function joinGameWithEth(uint256 _gameId, uint256 _playerRating) public payable nonReentrant {
-        // Checks
-        require(msg.value > 0, "Please send Eth to enter");
-        require(msg.value == gameEntryPriceInEth, "Please send correct amount of Eth to enter");
->>>>>>> 9189b408491004e3b45ea73613692df75bda426b
         require(_gameId > 0 && _gameId <= gameId, "Please select a game to enter");
         require(_playerRating > 0 && _playerRating < 101, "Please provide a rating of between 1 - 100");
 
         // Effects
-<<<<<<< HEAD
         games[_gameId].playerAddressesToRatings[msg.sender] = _playerRating; // Map player address to their rating within the selected game
         games[_gameId].gameBalance += msg.value; // calc value of msg.value - refund amount
         games[_gameId].numberOfPlayersInGame += 1;
@@ -200,28 +118,12 @@ contract R8R is Ownable, ReentrancyGuard {
 
         // emit event
         emit PlayerJoinedGame(msg.sender, _gameId, _playerRating, address(0));
-=======
-        // Add msg.sender to players array & _playerRating to playerRatings array
-        games[_gameId].players.push(msg.sender);
-        games[_gameId].playerRatings.push(_playerRating);
-
-        // Interactions - none, Eth accepted into contract
-
-        // emit event
-        emit PlayerJoinedGameWithEth(msg.sender, _gameId);
->>>>>>> 9189b408491004e3b45ea73613692df75bda426b
     }
 
     // @notice allows new players to join a selected game using ERC20 tokens
     // @param players must pass in an amount of an allow-listed token along with their selected _gameId & image rating
-<<<<<<< HEAD
     function _joinGameWithTokens(IERC20 _token, uint256 _amountOfToken, uint256 _gameId, uint256 _playerRating)
         internal
-=======
-    function joinGameWithTokens(IERC20 _token, uint256 _amountOfToken, uint256 _gameId, uint256 _playerRating)
-        public
-        nonReentrant
->>>>>>> 9189b408491004e3b45ea73613692df75bda426b
     {
         // Checks
         require(_amountOfToken > 0, "Please send a token amount of more than zero to enter");
@@ -230,7 +132,6 @@ contract R8R is Ownable, ReentrancyGuard {
         bool tokenAllowed;
         uint256 tokenEntryPrice;
 
-<<<<<<< HEAD
         if (gameTokensToEntryPrice[_token] > 0) {
             tokenAllowed = true;
             tokenEntryPrice = gameTokensToEntryPrice[_token];
@@ -238,45 +139,24 @@ contract R8R is Ownable, ReentrancyGuard {
 
         require(tokenAllowed == true, "Token sent is not allowed");
         require(_amountOfToken >= tokenEntryPrice, "Please send correct amount of tokens to enter");
-=======
-        for (uint256 i = 0; i < gameTokensAllowedList.length; i++) {
-            if (gameTokensAllowedList[i] == _token) {
-                tokenAllowed = true;
-                tokenEntryPrice = gameTokensEntryPriceList[i];
-            }
-        }
-
-        require(tokenAllowed == true, "Token sent is not allowed");
-
-        // check that the user has sent the correct price of entry in tokens
-        require(_amountOfToken == tokenEntryPrice, "Please send correct amount of tokens to enter");
-
->>>>>>> 9189b408491004e3b45ea73613692df75bda426b
         require(_gameId > 0 && _gameId <= gameId, "Please select a game to enter");
         require(_playerRating > 0 && _playerRating < 101, "Please provide a rating of between 1 - 100");
 
         // Effects
-<<<<<<< HEAD
         games[_gameId].playerAddressesToRatings[msg.sender] = _playerRating;
         games[_gameId].numberOfPlayersInGame += 1;
         games[_gameId].playerKeys.push(msg.sender);
 
         // *** ACCOUNTING FOR ERC20S REQUIRED ***
-=======
-        // Add msg.sender to players array & _playerRating to playerRatings array
-        games[_gameId].players.push(msg.sender);
-        games[_gameId].playerRatings.push(_playerRating);
->>>>>>> 9189b408491004e3b45ea73613692df75bda426b
 
         // Interactions
         // transfer tokens from player to contract (EOA approval required beforehand)
         bool success = _token.transferFrom(msg.sender, address(this), _amountOfToken);
         if (success != true) {
-            Error__TokenTransferFailed;
+            revert Error__TokenTransferFailed();
         }
 
         // emit event
-<<<<<<< HEAD
         emit PlayerJoinedGame(msg.sender, _gameId, _playerRating, address(_token));
     }
 
@@ -331,28 +211,14 @@ contract R8R is Ownable, ReentrancyGuard {
         if (gameTokensToEntryPrice[_token] > 0) {
             return gameTokensToEntryPrice[_token];
         }
-=======
-        emit PlayerJoinedGameWithTokens(msg.sender, _gameId);
-    }
-
-    function endGame(uint256 _robotRating) public onlyRobot {}
-
-    function addTokenToAllowedList(IERC20 _token) public onlyOwner {
-        gameTokensAllowedList.push(_token);
->>>>>>> 9189b408491004e3b45ea73613692df75bda426b
     }
 
     // =================
     // === MODIFIERS ===
     // =================
 
-<<<<<<< HEAD
     modifier onlyAi() {
         require(msg.sender == ai, "Only the AI can call this function");
-=======
-    modifier onlyRobot() {
-        require(msg.sender == robot, "Only the AI can call this function");
->>>>>>> 9189b408491004e3b45ea73613692df75bda426b
         _;
     }
 }
--- test/R8R.t.sol
+++ test/R8R.t.sol
@@ -3,20 +3,139 @@ pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {R8R} from "../src/R8R.sol";
+import {ERC20Mock} from "lib/openzeppelin-contracts/contracts/mocks/token/ERC20Mock.sol";
 
-contract CounterTest is Test {
+contract R8RTest is Test {
     R8R public r8r;
-<<<<<<< HEAD
-    address public ai = 0x1234567891234567891234567891234567891234; // real AI address is passed to the constructor
+    address public ai = makeAddr("ai");
+    address public user = makeAddr("user");
     uint256 public gameEntryPriceInEth = 1;
+    uint256 public STARTING_BALANCE = 10000;
+    ERC20Mock public allowedTestToken;
+
+    event GameCreated(uint256 indexed gameId, uint256 endTimestamp, uint256 gameEntryFee, uint256 prizePool);
+
+    error Error__TokenTransferFailed();
 
     function setUp() public {
         r8r = new R8R(ai, gameEntryPriceInEth);
-=======
-    address public robot = 0x1234567891234567891234567891234567891234; // real AI address is passed to the constructor
+        vm.deal(ai, STARTING_BALANCE);
+        allowedTestToken = new ERC20Mock();
+        allowedTestToken.mint(address(this), 50e18);
+        allowedTestToken.mint(user, 50e18);
+    }
 
-    function setUp() public {
-        r8r = new R8R(robot);
->>>>>>> 9189b408491004e3b45ea73613692df75bda426b
+    // =========================
+    // === CREATE GAME TESTS ===
+    // =========================
+
+    function testCreateGame_GameIdIncremented() public {
+        vm.startPrank(ai);
+        r8r.createGame();
+        // gameId should be '1' after initial game creation
+        assertEq(r8r.gameId(), 1);
+        vm.stopPrank();
+    }
+
+    // this test isn't working correctly - REVISIT!
+    function testCreateGame_ExpectEmitGameCreated() public {
+        vm.startPrank(ai);
+        r8r.createGame();
+        emit GameCreated(1, block.timestamp, 1, 1);
+        vm.stopPrank();
+    }
+
+    function testCreateGame_ExpectRevertIfNotAiCaller() public {
+        address hacker = makeAddr("hacker");
+        vm.startPrank(hacker);
+        vm.expectRevert("Only the AI can call this function");
+        r8r.createGame();
+        vm.stopPrank();
+    }
+
+    // =======================
+    // === JOIN GAME TESTS ===
+    // =======================
+
+    function testJoinGame_WithTokens_ExpectRevertIfTokenNotAllowed() public {
+        // create game first
+        vm.startPrank(ai);
+        r8r.createGame();
+        vm.stopPrank();
+
+        // set up mock token for user to send
+        ERC20Mock randToken = new ERC20Mock();
+        randToken.mint(user, 100e18);
+
+        // join game as user
+        vm.startPrank(user);
+        vm.expectRevert("Token sent is not allowed");
+        r8r.joinGame(randToken, 1234, 1, 35);
+        vm.stopPrank();
+    }
+
+    function testJoinGame_WithTokens_ExpectRevertIfNotEnoughTokensSent() public {
+        // owner adds token + price to allowed tokens
+        r8r.addTokenToAllowedList(allowedTestToken, 3);
+
+        // create game first
+        vm.startPrank(ai);
+        r8r.createGame();
+        vm.stopPrank();
+
+        // join game as user
+        vm.startPrank(user);
+        vm.expectRevert("Please send correct amount of tokens to enter");
+        r8r.joinGame(allowedTestToken, 2, 1, 35); // user has only sent 2 tokens when the price was set as 3 above
+        vm.stopPrank();
+    }
+
+    function testJoinGame_WithTokens_ExpectRevertIfGameIdDoesNotExist() public {
+        // owner adds token + price to allowed tokens
+        r8r.addTokenToAllowedList(allowedTestToken, 3);
+
+        // create game first
+        vm.startPrank(ai);
+        r8r.createGame();
+        vm.stopPrank();
+
+        // join game as user
+        vm.startPrank(user);
+        vm.expectRevert("Please select a game to enter");
+        r8r.joinGame(allowedTestToken, 4, 58, 35); // user has selected a game that doesn't exist: 58
+        vm.stopPrank();
+    }
+
+    function testJoinGame_WithTokens_ExpectRevertIfRatingIsTooHigh() public {
+        // owner adds token + price to allowed tokens
+        r8r.addTokenToAllowedList(allowedTestToken, 3);
+
+        // create game first
+        vm.startPrank(ai);
+        r8r.createGame();
+        vm.stopPrank();
+
+        // join game as user
+        vm.startPrank(user);
+        vm.expectRevert("Please provide a rating of between 1 - 100");
+        r8r.joinGame(allowedTestToken, 4, 1, 256); // user has selected a rating that is over 100: 256
+        vm.stopPrank();
+    }
+
+    // Test not passing - REVISIT!
+    function testJoinGame_WithTokens_ExpectErrorTokenTransferFailed() public {
+        // owner adds token + price to allowed tokens
+        r8r.addTokenToAllowedList(allowedTestToken, 3);
+
+        // create game first
+        vm.startPrank(ai);
+        r8r.createGame();
+        vm.stopPrank();
+
+        // join game as user
+        vm.startPrank(user);
+        vm.expectRevert(R8R.Error__TokenTransferFailed.selector);
+        r8r.joinGame(allowedTestToken, 4, 1, 35);
+        vm.stopPrank();
     }
 }
